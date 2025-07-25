f75cff66c85785fd764a75eb248e36610942260e
CuB1z <cub1zdev@gmail.com>
2025-07-25 11:33:44 +0200

    refactor: Minnor change to Logo.test.tsx

--- frontend/tests/components/shared/Logo.test.tsx
+++ frontend/tests/components/shared/Logo.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react"
 import Logo from "@components/shared/Logo"
+
+import { render, screen } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
 
 describe("Logo Component", () => {
