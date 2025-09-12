e34a31079d1a7008e8bae3173d7c2d526f3a984a
CuB1z <cub1zdev@gmail.com>
2025-09-12 09:31:04 +0200

    feat: Add Guide section to landing page

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -2,12 +2,14 @@ import Layout from "@/layouts/Layout";
 
 import Hero from "@/components/sections/Hero";
 import Features from "@/components/sections/Features";
+import Guide from "@/components/sections/Guide";
 
 export default function IndexPage() {
   return (
     <Layout>
       <Hero />
       <Features />
+      <Guide />
     </Layout>
   );
 }
