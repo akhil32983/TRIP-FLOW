3787d1e705347b87e6b51a8e22874a6d8509cc0b
CuB1z <cub1zdev@gmail.com>
2025-09-17 10:58:54 +0200

    feat: Integrate user authentication in Header component with conditional rendering

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -1,17 +1,26 @@
 import styles from "@styles/components/shared/Header.module.css";
 
+import { useAuth } from "@/providers/authProvider";
+
 import Button from "@components/shared/Button";
 import Logo from "@components/shared/Logo";
+import Avatar from "./Avatar";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.content}>
-                <Button to="/" style={["logo"]}>
-                    <Logo size="small" responsive />
-                </Button>
-                <Button style={["primary"]} label="Acceder" to="/login" />
-            </div>
-        </header>
-    );
+  const { user } = useAuth();
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <Button to="/" style={["logo"]}>
+          <Logo size="small" responsive />
+        </Button>
+        {!user ? (
+          <Button style={["primary"]} label="Acceder" to="/login" />
+        ) : (
+          <Avatar to="/dashboard" />
+        )}
+      </div>
+    </header>
+  );
 }
