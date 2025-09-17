3536f71c2a78c210ef2b04a2ba34798ac6f47287
CuB1z <cub1zdev@gmail.com>
2025-09-17 09:59:17 +0200

    feat: Add LoginPage component (not connected to backend yet)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
+import type { LoginRequest } from "@/types/auth";
+
+import Layout from "@/layouts/Layout";
+import AuthForm from "@/components/form/AuthForm";
 
 export default function LoginPage() {
-    return (
-        <h1>Login Page</h1>
-    );
+  const [errors, setErrors] = useState<Record<string, string> | null>(null);
+
+  const handleLogin = async (values: LoginRequest) => {
+    console.log(values);
+  };
+
+  return (
+    <Layout single>
+      <AuthForm
+        fields={[
+          { name: "username", label: "Usuario", placeholder: "CuB1z" },
+          {
+            name: "password",
+            label: "Contraseña",
+            type: "password",
+            placeholder: "********",
+          },
+        ]}
+        buttonLabel="Iniciar sesión"
+        onSubmit={(values: LoginRequest) => handleLogin(values)}
+        alternative={{
+          alternative: "¿No tienes una cuenta?",
+          alternativeLink: "/signup",
+          alternativeLabel: "Regístrate aquí",
+        }}
+        errors={errors}
+      />
+    </Layout>
+  );
 }
