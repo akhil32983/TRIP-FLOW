d84c4bf84cbb3900a0df39e7738ae2d4b8e3e9df
CuB1z <cub1zdev@gmail.com>
2025-09-19 09:38:07 +0200

    feat: Implement 404 page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,6 +1,40 @@
+import styles from "@styles/pages/NotFound.module.css";
+
+import { useAuth } from "@/providers/authProvider";
+
+import Layout from "@/layouts/Layout";
+import Logo from "@/components/shared/Logo";
+import Button from "@/components/shared/Button";
 
 export default function NotFoundPage() {
-    return (
-        <h1>404 - Page Not Found</h1>
-    );
+  const { user } = useAuth();
+  const targetRoute = user ? "/dashboard" : "/";
+
+  return (
+    <Layout single>
+      <section className={styles.notFound}>
+        <Logo size="large" />
+        <h1 className={styles.title}>
+          <strong>404 - Página no encontrada</strong>
+        </h1>
+        <h3 className={styles.subtitle}>
+          Parece que te has equivocado de ruta.
+        </h3>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>
+            Pero no te preocupes, incluso los mejores viajeros se pierden a
+            veces.
+          </p>
+          <p className={styles.text}>
+            Puedes volver al inicio y seguir planeando tu próxima aventura.
+          </p>
+        </div>
+        <Button
+          to={targetRoute}
+          style={["primary"]}
+          label="Volver a una ruta conocida"
+        ></Button>
+      </section>
+    </Layout>
+  );
 }
