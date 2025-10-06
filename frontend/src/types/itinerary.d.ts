ed58ecfcaacc45c3a797bf9d9deeeebb1d45faca
CuB1z <cub1zdev@gmail.com>
2025-10-06 09:36:24 +0200

    task: Define Itinerary, ItineraryDay, and Activity interfaces in types

--- frontend/src/types/itinerary.d.ts
+++ frontend/src/types/itinerary.d.ts
@@ -1,1 +1,36 @@
 export type ItineraryStatus = "ACTIVE" | "ONGOING" | "COMPLETED" | "DRAFT";
+
+export interface Activity {
+    activity: string;
+    details: string;
+    location: {
+        name: string;
+        address: string;
+        latitude: number;
+        longitude: number;
+    };
+    time: string;
+    duration: string;
+}
+
+export interface ItineraryDay {
+    day: number;
+    activities: Activity[];
+}
+
+export interface Itinerary {
+    id: number;
+    icon: string;
+    title: string;
+    place: string;
+    people: number;
+    budget: number;
+    date: string;
+    status: ItineraryStatus;
+    countDays: number;
+    tags: string[];
+}
+
+export interface ExtendedItinerary extends Itinerary {
+    days: ItineraryDay[];
+}
