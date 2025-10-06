0409dcaf7549c7a09c2e9952dc043ecab5ce1087
CuB1z <cub1zdev@gmail.com>
2025-10-06 09:12:11 +0200

    task: Update Itineraries page with new searchbar component

--- frontend/src/pages/itineraries/Itineraries.tsx
+++ frontend/src/pages/itineraries/Itineraries.tsx
@@ -1,9 +1,23 @@
-import AppLayout from "@/layouts/AppLayout";
+import { PlusIcon } from "lucide-react";
 
-export default function ItinerariesPage() {
-  return (
-    <AppLayout>
-        <h1>Itineraries Page (Protected)</h1>
-    </AppLayout>
-  );
+import AppLayout from "@/layouts/AppLayout";
+import Button from "@/components/shared/Button";
+import Searchbar from "@/components/shared/Searchbar";
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
+
+export default function Itineraries() {
+
+    return (
+        <AppLayout>
+            <DashboardHeader
+                title="Tus itinerarios"
+                responsiveRender={<Button to="/itineraries/new" style={["tool_bordered"]}><PlusIcon size={18} /></Button>}
+                defaultRender={<Button to="/itineraries/new" style={["primary"]} label="Nuevo itinerario"><PlusIcon size={18} /></Button>}
+            />
+            <Searchbar
+                placeHolder="Buscar itinerarios..."
+                onSearch={(query) => console.log("Search:", query)}
+            />
+        </AppLayout>
+    );
 }
