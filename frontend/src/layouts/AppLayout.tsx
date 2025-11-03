43e1d2c8304780920a0083b86804081bb1207cdb
CuB1z <cub1zdev@gmail.com>
2025-11-03 14:25:29 +0100

    feat: Add DemoBanner component to show user that the demo mode is active

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,18 +1,23 @@
 import styles from "@styles/layouts/AppLayout.module.css";
 
-import Sidebar from "@components/shared/Sidebar";
-
 import type { ReactNode } from "react";
+
+import { useDemo } from "@/providers/demoProvider";
+
+import Sidebar from "@components/shared/Sidebar";
+import DemoBanner from "@/components/shared/DemoBanner";
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 export default function AppLayout({ children }: LayoutProps) {
+    const { demo } = useDemo();
     return (
         <div className={styles.layout}>
             <Sidebar />
             <main className={styles.main}>
+                {demo && <DemoBanner rounded />}
                 {children}
             </main>
         </div>
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -2,8 +2,11 @@ import styles from "@styles/layouts/Layout.module.css";
 
 import type { ReactNode } from "react";
 
+import { useDemo } from "@/providers/demoProvider";
+
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import DemoBanner from "@/components/shared/DemoBanner";
 
 interface LayoutProps {
     single?: boolean;
@@ -11,8 +14,11 @@ interface LayoutProps {
 }
 
 export default function Layout({ single, children }: LayoutProps) {
+    const { demo } = useDemo();
+
     return (
         <div className={styles.layout}>
+            {demo && <DemoBanner />}
             {!single && <Header />}
             <main className={styles.main}>
                 {children}
