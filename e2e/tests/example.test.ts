ed9e3c80b10a32241285a65a5dc1d53a3c866d30
CuB1z <cub1zdev@gmail.com>
2025-11-11 11:01:10 +0100

    e2e-test: Remove example test

--- e2e/tests/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { FRONTEND_URL } from '../config/environment';
-
-test("Example Test", async ({ page }) => {
-    await page.goto(FRONTEND_URL);
-    await expect(page.getByText('Planifica tus viajes del futuro')).toBeVisible();
-});
