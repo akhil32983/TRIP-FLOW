3a8f7dbbacac4c2a334b0a786c6822ded2181271
CuB1z <cub1zdev@gmail.com>
2025-12-12 08:32:37 +0100

    feat: Implement AvatarUploader and set default avatar to first username char

---
+++ frontend/src/components/form/AvatarUploader.tsx
@@ -0,0 +1,66 @@
+import styles from "@styles/components/form/AvatarUploader.module.css";
+
+import { useRef, useState } from "react";
+
+import { API_BASE_URL } from "@/config/environment";
+import { useDemo } from "@/providers/demoProvider";
+
+import { Camera } from "lucide-react";
+
+interface AvatarUploaderProps {
+    username?: string;
+    preview?: string | null;
+    onFileSelect: (file: File) => void;
+}
+
+export default function AvatarUploader({
+    username, preview, onFileSelect
+}: AvatarUploaderProps) {
+    const { demo } = useDemo();
+
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [imgError, setImgError] = useState(false);
+
+    const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            onFileSelect(file);
+            setImgError(false);
+        }
+    };
+
+    const handleClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const src = preview || (username ? `${API_BASE_URL}/api/v1/users/${username}/avatar` : "");
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper} onClick={handleClick}>
+                {(!imgError && src && !demo) ? (
+                    <img 
+                        key={src}
+                        src={src} 
+                        className={styles.avatar} 
+                        onError={() => setImgError(true)}
+                        onLoad={() => setImgError(false)} 
+                        alt="Avatar"
+                    />
+                ) : (
+                    <div className={styles.placeholder}>
+                        {username?.charAt(0).toUpperCase() || "U"}
+                    </div>
+                )}
+                <div className={styles.overlay}><Camera size={32} /></div>
+            </div>
+            <input 
+                ref={fileInputRef} 
+                type="file" 
+                className={styles.input} 
+                onChange={handleFile} 
+                accept="image/*" 
+            />
+        </div>
+    );
+}
--- frontend/src/components/shared/Avatar.tsx
+++ frontend/src/components/shared/Avatar.tsx
@@ -5,19 +5,29 @@ import { Link } from "react-router";
 import { useAuth } from "@/providers/authProvider";
 import { useState } from "react";
 import { API_BASE_URL } from "@/config/environment";
+import { useDemo } from "@/providers/demoProvider";
 
 interface AvatarProps {
-    to: string;
+    to?: string;
     size?: "default" | "full";
 }
 
-export default function Avatar({ to, size = "default" }: AvatarProps) {
+export default function Avatar({ to = "#", size = "default" }: AvatarProps) {
     const [error, setError] = useState(false);
 
     const { user } = useAuth();
+    const { demo } = useDemo();
 
     const avatarClass = `${styles.avatar} ${size === "full" ? styles.full : ""}`;
     const src = `${API_BASE_URL}/api/v1/users/${user?.username}/avatar`;
+
+    if (demo) return (
+        <Link to={to} className={avatarClass}>
+            <div className={styles.placeholder}>
+                {user?.username.charAt(0).toUpperCase() || "U"}
+            </div>
+        </Link>
+    );
 
     return (
         <Link to={to} className={avatarClass}>
@@ -31,7 +41,7 @@ export default function Avatar({ to, size = "default" }: AvatarProps) {
             )}
             {error && (
                 <div className={styles.placeholder}>
-                    {user?.name.charAt(0).toUpperCase() || "U"}
+                    {user?.username.charAt(0).toUpperCase() || "U"}
                 </div>
             )}
         </Link>
