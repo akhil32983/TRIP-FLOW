72badf543d9bc00806d04931168069b916db83f1
CuB1z <cub1zdev@gmail.com>
2026-01-27 11:58:33 +0100

    ci: Fix ci pipelines

--- backend/api-service/src/main/java/com/tripflow/config/CorsConfig.java
+++ backend/api-service/src/main/java/com/tripflow/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${origins.frontend}")
+    @Value("${origins.frontend:http://localhost}")
     private String frontendOrigin;
 
     @Bean
